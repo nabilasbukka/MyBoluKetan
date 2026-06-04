dcd7fff10e202c87f163fb26edb87dc7217c1959
Nabila Putri Syafrina Bukka <nabilasyafrina.business@gmail.com>
2026-06-04 15:17:11 +0700

    Initial Commit

---
+++ MyBoluKetan/MyBoluKetanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBoluKetanApp.swift
+//  MyBoluKetan
+//
+//  Created by Nabila Putri Syafrina Bukka on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBoluKetanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
