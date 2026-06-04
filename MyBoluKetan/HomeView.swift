932f2610e4b676df10045dd0b16ffc19187ebb41
sol richas <stoedirman@gmail.com>
2026-06-04 15:45:02 +0700

    add home page

---
+++ MyBoluKetan/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MyBoluKetan
+//
+//  Created by Sholihatul Richas on 04/06/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home Page")
+    }
+}
+
+#Preview {
+    HomeView()
+}
